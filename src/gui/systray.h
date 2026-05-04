fcefb141ed7d36efe0f2f17610a7aa8214b577c6
Elysia <a.elysia@proton.me>
2026-05-04 17:53:11 +0800

    fix: remove encryption token discovery dialog implementation from systray
    Signed-off-by: Elysia <a.elysia@proton.me>

--- src/gui/systray.h
+++ src/gui/systray.h
@@ -127,9 +127,6 @@ public slots:
     void createEditFileLocallyLoadingDialog(const QString &fileName);
     void destroyEditFileLocallyLoadingDialog();
     void createResolveConflictsDialog(const OCC::ActivityList &allConflicts);
-    void createEncryptionTokenDiscoveryDialog();
-    void destroyEncryptionTokenDiscoveryDialog();
-
     void slotCurrentUserChanged();
 
     void forceWindowInit(QQuickWindow *window) const;
@@ -213,7 +210,6 @@ private:
 
     QSet<qint64> _callsAlreadyNotified;
     QPointer<QObject> _editFileLocallyLoadingDialog;
-    QPointer<QObject> _encryptionTokenDiscoveryDialog;
     QVector<QQuickWindow*> _fileDetailDialogs;
 
     QStringListModel _fakeActivityModel;
