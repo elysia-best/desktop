e29d1b493fe77fc81d4fa1fea8f0a14c59bee588
Elysia <a.elysia@proton.me>
2026-05-05 08:53:58 +0800

    fix: add missing config.h include in macOS file provider source files
    Signed-off-by: Elysia <a.elysia@proton.me>

--- src/gui/macOS/fileproviderservice.mm
+++ src/gui/macOS/fileproviderservice.mm
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#include "config.h"
 #include "fileproviderservice.h"
 
 #import <Foundation/Foundation.h>
--- src/gui/macOS/fileproviderxpc_mac.mm
+++ src/gui/macOS/fileproviderxpc_mac.mm
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#include "config.h"
 #include "common/utility.h"
 #include "fileproviderxpc.h"
 
